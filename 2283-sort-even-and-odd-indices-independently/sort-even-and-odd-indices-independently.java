1eca28f61ed346e222b1efadad8e870d469e3e7a
GummuduBhargavaraju <bhargavgummudu@gmail.com>
2025-05-01 22:58:31 +0530

    Time: 2 ms (96.51%) | Memory: 45.2 MB (5.34%) - LeetSync

---
+++ 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] sortEvenOdd(int[] nums) {
+        
+        int [] odds = new int [nums.length/2];
+            int [] evens = new int [nums.length - odds.length];
+         for(int i = 0 , x = 0 , y = 0; i<nums.length ; i++)
+        {
+            if (i %2 == 0)
+            {
+                evens[x++] = nums[i];  
+            }
+            else{
+                odds[y++] = nums[i];
+            }
+        }
+        Arrays.sort(odds);
+        Arrays.sort(evens);
+        for(int i = 0 , x = 0, y = odds.length-1 ;i<nums.length;i++)
+        {
+            if(i %2 == 0)
+            {
+              nums[i] =  evens[x++];
+               
+            }
+            else{
+                nums[i] = odds[y--];
+               
+            }
+        }
+        return nums;
+
+       
+       
+    }
+}
