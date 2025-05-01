89da757c4b28fc72cba4cb736305064938be6efe
GummuduBhargavaraju <bhargavgummudu@gmail.com>
2025-05-01 23:03:50 +0530

    Time: 2 ms (96.51%) | Memory: 45 MB (9.16%) - LeetSync

--- 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.java
+++ 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] sortEvenOdd(int[] nums) {
         
         int [] odds = new int [nums.length/2];
-            int [] evens = new int [nums.length - odds.length];
+            int [] evens = new int [nums.length -odds.length];
          for(int i = 0 , x = 0 , y = 0; i<nums.length ; i++)
         {
             if (i %2 == 0)
@@ -33,3 +33,4 @@ class Solution {
        
     }
 }
+
